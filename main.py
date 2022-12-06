890462782a7951302fc85c2963ca5444f04afb30
Maulana Rafi <109366541+termaulmaul@users.noreply.github.com>
2022-12-06 21:37:48 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# jumlah kopi yang dikonsumsi (dalam cangkir)
+coffee_amount = 2
+
+# konsentrasi kafein dalam kopi (dalam miligram per cangkir)
+caffeine_concentration = 100
+
+# hitung jumlah kafein yang dikonsumsi
+caffeine_amount = coffee_amount * caffeine_concentration
+
+# tampilkan jumlah kafein yang dikonsumsi
+print(f'Jumlah kafein yang dikonsumsi: {caffeine_amount} mg')
